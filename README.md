f7fc6550a364e0f7b9498c40ba356fa3d770cbda
mattdesl <dave.des@gmail.com>
2017-01-05 11:12:08 -0500

    bam!

---
+++ README.md
@@ -0,0 +1,13 @@
+# livereload-tests
+
+[![experimental](http://badges.github.io/stability-badges/dist/experimental.svg)](http://github.com/badges/stability-badges)
+
+
+
+## Usage
+
+[![NPM](https://nodei.co/npm/livereload-tests.png)](https://www.npmjs.com/package/livereload-tests)
+
+## License
+
+MIT, see [LICENSE.md](http://github.com/mattdesl/livereload-tests/blob/master/LICENSE.md) for details.
---
+++ server.js
@@ -0,0 +1,59 @@
+var budo = require('budo');
+var ws = require('ws');
+var WebSocketServer = ws.Server;
+var path = require('path');
+var browserify = require('browserify');
+var clientFile = path.resolve(__dirname, 'livereload-client.js');
+
+var wss;
+var reloadOnStartup = false;
+var appDir = __dirname;
+
+budo.cli(process.argv.slice(2), {
+  middleware: middleware
+}).on('connect', function (ev) {
+  console.log('LiveReload connected.');
+  wss = new WebSocketServer({ server: ev.server });
+  if (reloadOnStartup) {
+    wss.once('connection', function () {
+      reload();
+    });
+  }
+}).on('pending', function () {
+  reload();
+}).watch('**/*.{html,css}').on('watch', function (ev, file) {
+  var url = path.relative(appDir, file).replace(new RegExp(path.sep, 'g'), '/');
+  var ext = path.extname(file);
+  if (ext === '.css') {
+    console.log("TRIGGER CSS", url);
+    broadcast({ event: 'css', url: url });
+  } else {
+    reload();
+  }
+});
+
+function reload () {
+  if (!wss) return;
+  broadcast({ event: 'reload' });
+}
+
+function broadcast (data) {
+  if (!wss) return;
+  data = JSON.stringify(data);
+  wss.clients.forEach(function (client) {
+    client.send(data);
+  });
+}
+
+function middleware (req, res, next) {
+  if (req.url === '/livereload.js') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/javascript');
+
+    var b = browserify({ debug: false });
+    b.add(clientFile);
+    b.bundle().pipe(res);
+  } else {
+    next(null);
+  }
+}
